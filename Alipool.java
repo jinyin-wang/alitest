99fe96b4b1a28e3ece52c9c76e50f71c98aec4c6
jinyin-wang <1261162008@qq.com>
2020-04-27 21:44:41 -0400

    Update Alipool.java

--- Alipool.java
+++ Alipool.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Alipool {
-
-
+    //use bi-search to find the index of target
     private int findIndex(int[][] arr, int target) {
         int l = 0, r = arr.length;
         while(l < r) {
@@ -16,21 +15,19 @@ public class Alipool {
         return r;
     }
 
-
     private void alipool(int[][] arr, int allocatFund) {
-        System.out.println("hello");
-        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
-
+        //sorting the input ascendingly
+        Arrays.sort(arr, (a, b) -> b[0] - a[0]); 
         int remFund = allocatFund;
         int curLevel = arr[0][0];
         while(true) {
             curLevel -= 1;
             int index = findIndex(arr, curLevel);
-            // 如果剩余资金多于index*1， 减去index*1
+            // if the left fund is more than index*1， deduce index*1
             if(remFund > index) {
                 remFund -= index;
                 for (int i = 0; i < index; i++) arr[i][0] -= 1;
-            } else break; //否则减去index个1
+            } else break; //other wise deduce index*1
         }
 
         for(int i = 0; i < arr.length; i++)
@@ -41,6 +38,7 @@ public class Alipool {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        //saved as an array
         int[][] arr = new int[n][2];
         for(int i = 0; i < n; i++){
             arr[i][0] = sc.nextInt();
